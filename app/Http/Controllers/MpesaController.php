458cb78e451eb7ae35f9f68eb21bfb24bad8b13b
WGMI <imo.muganda21@gmail.com>
2022-01-28 15:07:06 +0300

    Minor bug fix

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -14,7 +14,7 @@ class MpesaController extends Controller
         $secret = "YxpQ7FTVXvP6AGN2";
         $businessCode = "174379";
         $passKey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
-        $callback = url("callback");
+        $callback = url("api/callback");
 
         Log::info($callback);
 
