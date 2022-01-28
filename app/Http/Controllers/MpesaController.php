aacb86d5cca444e7dd045751d2cfd4850114c3ba
WGMI <imo.muganda21@gmail.com>
2022-01-28 09:37:16 +0300

    Minor bug fix

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -14,7 +14,7 @@ class MpesaController extends Controller
         $passKey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
         $callback = url("callback");
 
-        $phone_number = $number;
+        $phone_number = '254' . ltrim($number, $number[0]);
         $accountRef = "qikapu";
         $desc = "Qikapu payment";
         $amount = $amount_to_pay;
