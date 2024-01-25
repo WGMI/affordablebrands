781c65b39fbf2de8e4392b398a4bf1536a06c29c
WGMI <imo.muganda21@gmail.com>
2024-01-25 18:13:58 +0300

    Mail

--- app/Http/Controllers/TempController.php
+++ app/Http/Controllers/TempController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use App\Mail\MLTEmail;
 
 class TempController extends Controller
 {
     public function sendEmail(Request $request){
         $recipientName = $request->name; // Replace with the recipient's name
 
-        Mail::to($request->email)->send(new MilletQuestEmail($recipientName));
+        Mail::to($request->email)->send(new MLTEmail($recipientName));
 
         return "Email sent successfully!";
     }
